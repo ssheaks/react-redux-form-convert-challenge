84ed18386a83ced5ecb329998c71a5aa9424e5ae
Joe Turner <joe@oampo.co.uk>
2017-11-30 17:57:44 +0000

    Add solution

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-export default function ContactForm() {
+export function ContactForm() {
     return (
         <form>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" />
+            <Field type="text" id="name" name="name" component="input" />
             <label htmlFor="email">Email address</label>
-            <input type="email" id="email" />
+            <Field type="email" id="email" name="email" component="input" />
             <label htmlFor="message">Message</label>
-            <textarea id="message" />
+            <Field id="message" name="message" component="textarea" />
             <button>Submit</button>
         </form>
     );
 }
+
+export default reduxForm({
+    form: 'contact'
+})(ContactForm);
